03611b750a7e52f3008f5904d1bf1c256c60d783
Thomas Broadwater <tgbroadh2o@gmail.com>
2022-10-05 12:29:04 -0400

    3d

--- src/sketch.js
+++ src/sketch.js
@@ -2,7 +2,6 @@ let sides = 3;
 function setup() {
   let currentCanvas = createCanvas(400, 400, WEBGL);
   currentCanvas.parent('container');
-  strokeWeight(3);
 }
 
 function draw() {
@@ -11,6 +10,10 @@ function draw() {
   rotateZ(frameCount * 0.01);
   fill('#cb3028');
   torus(50, 20, sides, 24);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  rotateZ(frameCount * 0.01);
+  torus(150, 30, 30, 30);
 }
 
 function mouseClicked() {
