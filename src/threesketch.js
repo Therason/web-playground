5068cadb8f69f70e646695989de13fe50fe04b7c
Thomas Broadwater <tgbroadh2o@gmail.com>
2022-10-11 22:34:54 -0400

    Port p5 stuff to threesketch.js

--- src/threesketch.js
+++ src/threesketch.js
@@ -1,27 +1,41 @@
 // three.js needs THREE things:
 // a scene, a camera, and a renderer
 const scene = new THREE.Scene();
-// renderer's size is a square, so aspect ratio can be 1
+scene.background = new THREE.Color(0x636c38)
+// canvas' size is a square, so aspect ratio can be 1
 const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-
 renderer.setSize(400, 400);
 document.getElementById("container").appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x636c38});
-const cube = new THREE.Mesh(geometry, material);
+// torus
+let sides = 3;
+let geometry = new THREE.TorusGeometry(15, 3, 24, sides);
+const material = new THREE.MeshBasicMaterial({color: 0xcb3028});
+let torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
 
-scene.add(cube);
-camera.position.z = 5;
-
+camera.position.z = 40;
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
 
   renderer.render(scene, camera);
-}
-
+};
 animate();
+
+const canv = document.querySelector("canvas");
+canv.addEventListener("click", () => {
+  if (sides > 10) {
+    sides = 3;
+  } else {
+    sides++;
+  }
+  scene.remove(torus);
+  geometry.dispose();
+  geometry = new THREE.TorusGeometry(15, 3, 24, sides);
+  torus = new THREE.Mesh(geometry, material);
+  scene.add(torus);
+});
